58e8865077f410fe70900a04686c8db7c291acd0
Ana Paula Lopes <30847848+anaplopes@users.noreply.github.com>
2020-02-05 12:06:19 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -7,6 +7,6 @@ class ConnectMongo:
         self.client = MongoClient('mongodb://localhost:27017/') #conecta no banco de dados
         
     def connect_db(self, db, collection):
-        self.client[db] #acessa o banco de dados
-        session = db[collection] #acessa a minha coleção dentro desse banco de dados
+        _db = self.client[db] #acessa o banco de dados
+        session = _db[collection] #acessa a minha coleção dentro desse banco de dados
         return session
